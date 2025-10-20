06a8c0e58fd8f9115d622f78571395d8bd4cad3c
cludley <cludley@anamosa.k12.ia.us>
2025-10-20 13:17:24 -0500

    added double brackets

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let groupedNumbers = ["1", "3", "5"] ["2", "4", "6"];
+let groupedNumbers = [["1", "3", "5"] ["2", "4", "6"]];
 
 document.getElementById('result').innerHTML = groupedNumbers;
+
 
 
 
